9a55cfbe43e08814131ff27e040a529ff3298145
Ali Doustkani <ali.doostkany@gmail.com>
2018-10-08 16:34:51 +0330

    aspnetcore project creaetd

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index()
+        {
+            return View("About");
+        }
+    }
+}
