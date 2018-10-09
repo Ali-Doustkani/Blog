e3b9cea502d6bee122436ca0576e8972185dfe29
Ali Doustkani <ali.doostkany@gmail.com>
2018-10-09 13:28:39 +0330

    styles refactored to sass

---
+++ src/gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require("gulp");
+var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+
+gulp.task("sass", function () {
+    var options = {
+        outputStyle: "expanded"
+    };
+    return gulp.src("./Styles/Site.scss")
+        .pipe(sass(options))
+        .pipe(rename("~site.css"))
+        .pipe(gulp.dest("./wwwroot/css"));
+});
