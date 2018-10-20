858f0ceae476334a998414febfb20a84746dec80
Ali Doustkani <ali.doostkany@gmail.com>
2018-10-20 18:28:54 +0330

    gulpfile changed for final build

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,11 +3,16 @@ var sass = require("gulp-sass");
 var rename = require("gulp-rename");
 
 gulp.task("sass", function () {
-    var options = {
-        outputStyle: "expanded"
-    };
+    return sassTask({ outputStyle: "expanded" });
+});
+
+gulp.task("build", function () {
+    return sassTask({ outputStyle: "compressed" });
+});
+
+function sassTask(options) {
     return gulp.src("./Styles/Site.scss")
         .pipe(sass(options))
         .pipe(rename("~site.css"))
         .pipe(gulp.dest("./wwwroot/css"));
-});
+}
