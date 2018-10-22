b7f458af43538412af1965db770afb6adde8ff3e
Ali Doustkani <ali.doostkany@gmail.com>
2018-10-22 14:58:57 +0330

    save to db & redirect

--- src/Controllers/AdministratorController.cs
+++ src/Controllers/AdministratorController.cs
@@ -1,15 +1,19 @@
 ﻿using Blog.Model;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Blog.Controllers
 {
     [Route("admin")]
     public class AdministratorController : Controller
     {
+        public AdministratorController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        private readonly BlogContext _context;
+
         [Route("post")]
         public ViewResult Post()
         {
@@ -18,6 +22,15 @@ namespace Blog.Controllers
                 PublishDate = DateTime.Now
             };
             return View(newPost);
+        }
+
+        [Route("post/save")]
+        [HttpPost]
+        public IActionResult Save(Post post)
+        {
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+            return Redirect("~/blog/post/" + post.Id.ToString());
         }
     }
 }
