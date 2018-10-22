c195b01f904bcecef53b1898189de1db7593bb5e
Ali Doustkani <ali.doostkany@gmail.com>
2018-10-22 14:41:17 +0330

    post form view

---
+++ src/Controllers/AdministratorController.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    [Route("admin")]
+    public class AdministratorController : Controller
+    {
+        [Route("post")]
+        public ViewResult Post()
+        {
+            var newPost = new Post
+            {
+                PublishDate = DateTime.Now
+            };
+            return View(newPost);
+        }
+    }
+}
--- src/Startup.cs
+++ src/Startup.cs
@@ -29,7 +29,7 @@ namespace Blog
         {
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc();
         }
     }
 }
