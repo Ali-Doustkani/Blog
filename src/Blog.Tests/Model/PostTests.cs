7d5d78fb5718ede8db56138653839f85efdb7cc7
Ali Doustkani <ali.doostkany@gmail.com>
2018-10-22 17:05:14 +0330

    tests fix

---
+++ src/Blog.Tests/Model/PostTests.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Model;
+using System.Linq;
+using Xunit;
+
+namespace Blog.Tests.Model
+{
+    public class PostTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void TagCollection_WithEmptyString_EmptyCollection(string tags)
+        {
+            var post = new Post { Tags = tags };
+
+            Assert.Equal(Enumerable.Empty<string>(), post.TagCollection);
+        }
+
+        [Theory]
+        [InlineData(",,a,b")]
+        [InlineData("a,,,,c")]
+        [InlineData("a,b,")]
+        public void TagCollection_IgnoresEmptyStringParts(string tags)
+        {
+            var post = new Post { Tags = tags };
+
+            Assert.Equal(2, post.TagCollection.Count());
+        }
+    }
+}
