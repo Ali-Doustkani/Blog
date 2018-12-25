5e912ff6e8b991f71eccc0fa814036460bfffecd
Ali Doustkani <ali.doostkany@gmail.com>
2018-12-25 20:26:19 +0330

    use persianCalendar for farsi dates

--- src/Blog.Tests/ExtensionsTests.cs
+++ src/Blog.Tests/ExtensionsTests.cs
@@ -10,5 +10,17 @@ namespace Blog.Tests
         {
             Assert.Equal("Home", Extensions.NameOf<HomeController>());
         }
+
+        [Fact]
+        public void GetShortPersianDate()
+        {
+            Assert.Equal("دی 1397", Extensions.GetShortPersianDate(new System.DateTime(2018, 12, 25)));
+        }
+
+        [Fact]
+        public void GetLongPersianDate()
+        {
+            Assert.Equal("سه شنبه، 4 دی 1397", Extensions.GetLongPersianDate(new System.DateTime(2018, 12, 25)));
+        }
     }
 }
--- src/Blog/Extensions.cs
+++ src/Blog/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Globalization;
 
 namespace Blog
 {
@@ -7,6 +8,53 @@ namespace Blog
         public static string NameOf<T>( )
         {
             return typeof(T).Name.Replace("Controller", string.Empty);
+        }
+
+        public static string GetShortPersianDate(DateTime date)
+        {
+            var cal = new PersianCalendar();
+            return $"{MonthName(cal.GetMonth(date))} {cal.GetYear(date)}";
+        }
+
+        public static string GetLongPersianDate(DateTime date)
+        {
+            var cal = new PersianCalendar();
+            return $"{DayName(cal.GetDayOfWeek(date))}، {cal.GetDayOfMonth(date)} {MonthName(cal.GetMonth(date))} {cal.GetYear(date)}";
+        }
+
+        private static object DayName(DayOfWeek dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Friday: return "جمعه";
+                case DayOfWeek.Monday: return "دوشنبه";
+                case DayOfWeek.Saturday: return "شنبه";
+                case DayOfWeek.Sunday: return "یکشنبه";
+                case DayOfWeek.Thursday: return "پنج شنبه";
+                case DayOfWeek.Tuesday: return "سه شنبه";
+                case DayOfWeek.Wednesday: return "چهارشنبه";
+            }
+            throw new ArgumentException(nameof(dayOfWeek));
+        }
+
+        private static string MonthName(int month)
+        {
+            switch (month)
+            {
+                case 1: return "فروردین";
+                case 2: return "اردیبهشت";
+                case 3: return "خرداد";
+                case 4: return "تیر";
+                case 5: return "مرداد";
+                case 6: return "شهریور";
+                case 7: return "مهر";
+                case 8: return "آبان";
+                case 9: return "آذر";
+                case 10: return "دی";
+                case 11: return "بهمن";
+                case 12: return "اسفند";
+            }
+            throw new ArgumentException(nameof(month));
         }
     }
 }
