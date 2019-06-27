c3b774f3ab4ca27121583b3ddbc5d50793af5da2
Ali <ali.doostkany@gmail.com>
2019-06-27 17:32:59 +0430

    cleanup post delete

---
+++ src/Blog/Views/Administrator/Index.js
@@ -0,0 +1,6 @@
+﻿document.querySelectorAll(".delete-button").forEach(btn => btn.addEventListener("click", ask));
+function ask(e) {
+    if (!confirm("Are you sure you want to delete this post?")) {
+        e.preventDefault();
+    }
+}
