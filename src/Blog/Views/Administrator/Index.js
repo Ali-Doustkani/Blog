8e0e50596b714957a9e8b3f4125aaa64a2c58806
Ali <ali.doostkany@gmail.com>
2019-06-28 12:16:53 +0430

    enable richtext

--- src/Blog/Views/Administrator/Index.js
+++ src/Blog/Views/Administrator/Index.js
@@ -1,33 +1,6 @@
-﻿import { create } from '@alidoustkani/richtext';
-
-document.querySelectorAll(".delete-button").forEach(btn => btn.addEventListener("click", ask));
+﻿document.querySelectorAll(".delete-button").forEach(btn => btn.addEventListener("click", ask));
 function ask(e) {
     if (!confirm("Are you sure you want to delete this post?")) {
         e.preventDefault();
     }
-}
-
-const editor = document.getElementById("editor");
-console.log(editor);
-if (editor) {
-    create(editor, {
-        defaultLink: "/",
-        staySelected: true,
-        decors: {
-            important: 'strong',
-            box: {
-                parent: true,
-                tag: 'div',
-                className: 'box-wrapper'
-            },
-            note: {
-                tag: 'span',
-                className: 'note'
-            },
-            warning: {
-                tag: 'span',
-                className: 'warning'
-            }
-        }
-    });
 }
---
+++ src/Blog/Views/Administrator/Post.js
@@ -0,0 +1,65 @@
+﻿import { create } from '@alidoustkani/richtext';
+
+const saveForm = document.getElementById("save-post");
+const editor = document.getElementById("editor");
+saveForm.addEventListener("submit", () => {
+    console.log('start');
+    const input = document.createElement("input");
+    input.type = "hidden";
+    input.name = "Content";
+    input.value = editor.innerHTML;
+    saveForm.appendChild(input);
+});
+
+const richtext = create(editor, {
+    defaultLink: "/",
+    staySelected: false,
+    decors: {
+        important: 'strong',
+        h1: {
+            parent: true,
+            tag: "h3"
+        },
+        h2: {
+            parent: true,
+            tag: "h4"
+        },
+        code: {
+            parent: true,
+            tag: "pre",
+            className: "code"
+        },
+        terminal: {
+            parent: true,
+            tag: "pre",
+            className: "terminal"
+        },
+        note: {
+            parent: true,
+            tag: "div",
+            className: "note"
+        },
+        warning: {
+            parent: true,
+            tag: "div",
+            className: "warning"
+        }
+    }
+});
+
+const click = (id, func) => document.getElementById(id).addEventListener("click", e => {
+    e.preventDefault();
+    func();
+});
+
+click("important", () => richtext.style("important"));
+click("h1", () => richtext.apply("h1"));
+click("h2", () => richtext.apply("h2"));
+click("code", () => richtext.apply("code"));
+click("terminal", () => richtext.apply("terminal"));
+click("note", () => richtext.apply("note"));
+click("warning", () => richtext.apply("note"));
+click("unorderedList", () => richtext.applyUnorderedList());
+click("orderedList", () => richtext.applyOrderedList());
+click("image", () => richtext.selectImage());
+click("link", () => richtext.styleLink());
