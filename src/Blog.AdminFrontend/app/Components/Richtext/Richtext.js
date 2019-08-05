bac7e253b00ea8ab1e6476eaab3273036d4c1f7e
Ali <ali.doostkany@gmail.com>
2019-08-05 12:54:29 +0430

    rewrite richtext with react hooks

--- src/Blog.AdminFrontend/app/Components/Richtext/Richtext.js
+++ src/Blog.AdminFrontend/app/Components/Richtext/Richtext.js
@@ -1,81 +1,89 @@
-import React from 'react'
+import React, { useState, useRef, useMemo, useEffect } from 'react'
 import Button from './Button'
 import { create } from '@alidoustkani/richtext'
-import InnerRichtext from './OriginalRichtext'
 import { ErrorList } from '../ErrorList'
 
-class Richtext extends React.Component {
-   constructor(props) {
-      super(props)
-      this.editorRef = React.createRef()
-      this.change = this.change.bind(this)
-      this.state = { hasFocus: false }
+const OPTIONS = {
+   defaultLink: '/',
+   staySelected: false,
+   decors: {
+      important: 'strong',
+      inlineCode: { tag: 'span', className: 'inline-code' },
+      h1: { parent: true, tag: 'h3' },
+      h2: { parent: true, tag: 'h4' },
+      code: { parent: true, tag: 'pre', className: 'code' },
+      terminal: { parent: true, tag: 'pre', className: 'terminal' },
+      note: { parent: true, tag: 'div', className: 'note' },
+      warning: { parent: true, tag: 'div', className: 'warning' }
    }
-   componentDidMount() {
-      this.rich = create(this.editorRef.current, {
-         defaultLink: '/',
-         staySelected: false,
-         decors: {
-            important: 'strong',
-            inlineCode: { tag: 'span', className: 'inline-code' },
-            h1: { parent: true, tag: 'h3' },
-            h2: { parent: true, tag: 'h4' },
-            code: { parent: true, tag: 'pre', className: 'code' },
-            terminal: { parent: true, tag: 'pre', className: 'terminal' },
-            note: { parent: true, tag: 'div', className: 'note' },
-            warning: { parent: true, tag: 'div', className: 'warning' }
-         }
-      })
-   }
-   change(e) {
-      if (this.props.onChange) {
-         this.props.onChange(e)
-      }
-   }
-   render() {
-      const classes = ['entry']
-      let errorList = null
-      if (this.props.errors && this.props.errors.length) {
-         classes.push('incorrect')
-         errorList = <ErrorList errors={this.props.errors} />
-      }
-      if (this.state.hasFocus) {
-         classes.push('hasFocus')
-      }
+}
 
-      return (
-         <div className="text-group richtext-group">
-            {this.props.label ? <label>{this.props.label}</label> : null}
-            <div className="toolbar">
-               <Button content="bold" onClick={() => this.rich.style('important')} />
-               <Button content="code" onClick={() => this.rich.style('inlineCode')} />
-               <div className="separator" />
-               <Button content={<strong>H1</strong>} onClick={() => this.rich.apply('h1')} />
-               <Button content={<strong>H2</strong>} onClick={() => this.rich.apply('h2')} />
-               <div className="separator" />
-               <Button content="code" onClick={() => this.rich.apply('code')} />
-               <Button content="terminal" onClick={() => this.rich.apply('terminal')} />
-               <div className="separator" />
-               <Button content="exclamation" onClick={() => this.rich.apply('note')} />
-               <Button content="times" onClick={() => this.rich.apply('warning')} />
-               <div className="separator" />
-               <Button content="list-ul" onClick={() => this.rich.applyUnorderedList()} />
-               <Button content="list-ol" onClick={() => this.rich.applyOrderedList()} />
-               <div className="separator" />
-               <Button content="camera" onClick={() => this.rich.selectImage()} />
-               <Button content="link" onClick={() => this.rich.styleLink()} />
-            </div>
-            <div
-               className={classes.join(' ')}
-               onFocus={() => this.setState({ hasFocus: true })}
-               onBlur={() => this.setState({ hasFocus: false })}
-            >
-               <InnerRichtext ref={this.editorRef} {...this.props} />
-            </div>
-            {errorList}
-         </div>
-      )
+function Richtext(props) {
+   const editorRef = useRef()
+   const [rich, setRich] = useState(null)
+   const [hasFocus, setFocus] = useState(false)
+
+   useEffect(() => setRich(create(editorRef.current, OPTIONS)), [])
+
+   const article = useMemo(
+      () => (
+         <article
+            data-testid={props['data-testid']}
+            ref={editorRef}
+            className="entry"
+            dangerouslySetInnerHTML={{ __html: props.innerHtml }}
+            onBlur={() =>
+               props.onChange({
+                  target: {
+                     name: props.name,
+                     value: editorRef.current.innerHTML
+                  }
+               })
+            }
+         />
+      ),
+      []
+   )
+   const classes = ['entry']
+   if (props.errors && props.errors.length) {
+      classes.push('incorrect')
    }
+   if (hasFocus) {
+      classes.push('hasFocus')
+   }
+
+   return (
+      <div className="text-group richtext-group">
+         {props.label ? <label>{props.label}</label> : null}
+         <div className="toolbar">
+            <Button content="bold" onClick={() => rich.style('important')} />
+            <Button content="code" onClick={() => rich.style('inlineCode')} />
+            <div className="separator" />
+            <Button content={<strong>H1</strong>} onClick={() => rich.apply('h1')} />
+            <Button content={<strong>H2</strong>} onClick={() => rich.apply('h2')} />
+            <div className="separator" />
+            <Button content="code" onClick={() => rich.apply('code')} />
+            <Button content="terminal" onClick={() => rich.apply('terminal')} />
+            <div className="separator" />
+            <Button content="exclamation" onClick={() => rich.apply('note')} />
+            <Button content="times" onClick={() => rich.apply('warning')} />
+            <div className="separator" />
+            <Button content="list-ul" onClick={() => rich.applyUnorderedList()} />
+            <Button content="list-ol" onClick={() => rich.applyOrderedList()} />
+            <div className="separator" />
+            <Button content="camera" onClick={() => rich.selectImage()} />
+            <Button content="link" onClick={() => rich.styleLink()} />
+         </div>
+         <div
+            className={classes.join(' ')}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+         >
+            {article}
+         </div>
+         <ErrorList errors={props.errors} />
+      </div>
+   )
 }
 
 export { Richtext }
