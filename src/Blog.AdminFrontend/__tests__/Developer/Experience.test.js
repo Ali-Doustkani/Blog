4fa71029db29044094845e93fb2c7b83b102ef7c
Ali <ali.doostkany@gmail.com>
2019-08-07 12:13:39 +0430

    unit test Experience

--- src/Blog.AdminFrontend/__tests__/Developer/Experience.test.js
+++ src/Blog.AdminFrontend/__tests__/Developer/Experience.test.js
@@ -1,86 +1,46 @@
 import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import { render, cleanup, fireEvent } from '@testing-library/react'
+import { shallow } from 'enzyme'
 import Experience from '../../app/Developer/Experience'
 
-afterEach(cleanup)
+const sample = {
+   company: 'Bellin',
+   position: 'C# Developer',
+   startDate: '2018-03-29',
+   endDate: '2019-04-29',
+   content: '<p>Worked as a developer</p>'
+}
 
-it('display an experience', () => {
-   const sample = {
-      company: 'Bellin',
-      position: 'C# Developer',
-      startDate: '2018-03-29',
-      endDate: '2019-04-29',
-      content: '<p>Worked as a developer</p>'
-   }
-
-   const { getByDisplayValue, getByTestId, getByText } = render(<Experience {...sample} />)
-   expect(getByDisplayValue('Bellin')).toBeVisible()
-   expect(getByDisplayValue('C# Developer')).toBeVisible()
-   expect(getByTestId('startDate-input')).toHaveValue('2018-03-29')
-   expect(getByTestId('endDate-input')).toHaveValue('2019-04-29')
-   expect(getByText('Worked as a developer')).toBeVisible()
+it('displays company', () => {
+   const position = shallow(<Experience {...sample} />)
+      .find('Input')
+      .findWhere(x => x.prop('name') === 'company')
+   expect(position.prop('company')).toBe('Bellin')
 })
 
-it('call onChange when data is changed', () => {
-   const experience = {}
-   const { getByTestId } = render(<Experience onChange={e => Object.assign(experience, e)} />)
-
-   fireEvent.change(getByTestId('company-input'), { target: { value: 'Lodgify' } })
-   fireEvent.change(getByTestId('position-input'), { target: { value: 'C# Developer' } })
-   fireEvent.change(getByTestId('startDate-input'), { target: { value: '2012-01-10' } })
-   fireEvent.change(getByTestId('endDate-input'), { target: { value: '2014-10-20' } })
-   getByTestId('content-richtext').innerHTML = '<p>content</p>'
-   fireEvent.blur(getByTestId('content-richtext'))
-
-   expect(experience).toEqual({
-      company: 'Lodgify',
-      position: 'C# Developer',
-      startDate: '2012-01-10',
-      endDate: '2014-10-20',
-      content: '<p>content</p>'
-   })
+it('displays position', () => {
+   const position = shallow(<Experience {...sample} />)
+      .find('Input')
+      .findWhere(x => x.prop('name') === 'position')
+   expect(position.prop('position')).toBe('C# Developer')
 })
 
-it('make company field red when has error', () => {
-   let { getByTestId } = render(<Experience companyErrors={['error']} />)
-   expect(getByTestId('company-input')).toHaveClass('incorrect')
-   cleanup()
-   getByTestId = render(<Experience />).getByTestId
-   expect(getByTestId('company-input')).not.toHaveClass('incorrect')
+it('displays startDate', () => {
+   const position = shallow(<Experience {...sample} />)
+      .find('Input')
+      .findWhere(x => x.prop('name') === 'startDate')
+   expect(position.prop('startDate')).toBe('2018-03-29')
 })
 
-it('make position field red when has error', () => {
-   let { getByTestId } = render(<Experience positionErrors={['error']} />)
-   expect(getByTestId('position-input')).toHaveClass('incorrect')
-   cleanup()
-   getByTestId = render(<Experience />).getByTestId
-   expect(getByTestId('position-input')).not.toHaveClass('incorrect')
+it('displays endDate', () => {
+   const position = shallow(<Experience {...sample} />)
+      .find('Input')
+      .findWhere(x => x.prop('name') === 'endDate')
+   expect(position.prop('endDate')).toBe('2019-04-29')
 })
 
-it('make startDate field red when has error', () => {
-   let { getByTestId } = render(<Experience startDateErrors={['error1']} />)
-   expect(getByTestId('startDate-input')).toHaveClass('incorrect')
-   cleanup()
-   getByTestId = render(<Experience />).getByTestId
-   expect(getByTestId('startDate-input')).not.toHaveClass('incorrect')
-})
-
-it('make endDate field red when has error', () => {
-   let { getByTestId } = render(<Experience endDateErrors={['error']} />)
-   expect(getByTestId('endDate-input')).toHaveClass('incorrect')
-   cleanup()
-   getByTestId = render(<Experience />).getByTestId
-   expect(getByTestId('endDate-input')).not.toHaveClass('incorrect')
-})
-
-it('show list of all field errors', () => {
-   let { getByTestId } = render(
-      <Experience companyErrors={['err1', 'err2']} endDateErrors={['err3']} />
-   )
-   const list = getByTestId('error-list')
-   expect(list.childNodes.length).toBe(3)
-   expect(list.childNodes[0].textContent).toBe('err1')
-   expect(list.childNodes[1].textContent).toBe('err2')
-   expect(list.childNodes[2].textContent).toBe('err3')
+it('displays content', () => {
+   const position = shallow(<Experience {...sample} />)
+      .find('Richtext')
+      .findWhere(x => x.prop('name') === 'content')
+   expect(position.prop('content')).toBe('<p>Worked as a developer</p>')
 })
--- src/Blog.AdminFrontend/jest.config.js
+++ src/Blog.AdminFrontend/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
    setupFilesAfterEnv: ['<rootDir>__tests__/setupTests.js'],
    transformIgnorePatterns: ['node_modules/(?!(@alidoustkani/richtext)/)'],
-   testPathIgnorePatterns: ['__mocks__'],
+   testPathIgnorePatterns: ['__mocks__', 'setupTest'],
    moduleNameMapper: {
       '\\.(svg|jpg|png)$': '<rootDir>/__tests__/__mocks__/empty-module.js'
    }
