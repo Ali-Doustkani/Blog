d0c60d2b816ed960c1ac630d8b8685d6fff85644
Ali <ali.doostkany@gmail.com>
2019-08-12 17:54:06 +0430

    use components in enzyme

--- src/Blog.AdminFrontend/src/app/components/Developer/Experience.test.js
+++ src/Blog.AdminFrontend/src/app/components/Developer/Experience.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Experience from './Experience'
+import { Input, Richtext } from 'Controls'
 
 const sample = {
    company: 'Bellin',
@@ -12,35 +13,35 @@ const sample = {
 
 it('displays company', () => {
    const position = shallow(<Experience {...sample} />)
-      .find('Input')
+      .find(Input)
       .findWhere(x => x.prop('name') === 'company')
    expect(position.prop('company')).toBe('Bellin')
 })
 
 it('displays position', () => {
    const position = shallow(<Experience {...sample} />)
-      .find('Input')
+      .find(Input)
       .findWhere(x => x.prop('name') === 'position')
    expect(position.prop('position')).toBe('C# Developer')
 })
 
 it('displays startDate', () => {
    const position = shallow(<Experience {...sample} />)
-      .find('Input')
+      .find(Input)
       .findWhere(x => x.prop('name') === 'startDate')
    expect(position.prop('startDate')).toBe('2018-03-29')
 })
 
 it('displays endDate', () => {
    const position = shallow(<Experience {...sample} />)
-      .find('Input')
+      .find(Input)
       .findWhere(x => x.prop('name') === 'endDate')
    expect(position.prop('endDate')).toBe('2019-04-29')
 })
 
 it('displays content', () => {
    const position = shallow(<Experience {...sample} />)
-      .find('Richtext')
+      .find(Richtext)
       .findWhere(x => x.prop('name') === 'content')
    expect(position.prop('content')).toBe('<p>Worked as a developer</p>')
 })
--- src/Blog.AdminFrontend/src/app/components/Developer/ExperienceList.test.js
+++ src/Blog.AdminFrontend/src/app/components/Developer/ExperienceList.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import ExperienceList from './ExperienceList'
+import Experience from './Experience'
 
 it('displays an experience component for each experience object', () => {
    const data = [{ id: 11 }, { id: 22 }]
-   const list = shallow(<ExperienceList experiences={data} />).find('Experience')
+   const list = shallow(<ExperienceList experiences={data} />).find(Experience)
    expect(list.length).toBe(2)
    expect(list.findWhere(x => x.prop('id') === 11).length).toBe(1)
    expect(list.findWhere(x => x.prop('id') === 22).length).toBe(1)
@@ -14,7 +15,7 @@ it('calls for delete when an experience needs to be deleted', () => {
    expect.assertions(1)
    const data = [{ id: 1 }]
    const list = shallow(<ExperienceList experiences={data} onDelete={id => expect(id).toBe(1)} />)
-   list.find('Experience').prop('onDelete')(1)
+   list.find(Experience).prop('onDelete')(1)
 })
 
 it('calls for change when an experience is changed', () => {
@@ -23,12 +24,12 @@ it('calls for change when an experience is changed', () => {
    const list = shallow(
       <ExperienceList experiences={data} onChange={e => expect(e).toEqual({ id: 1 })} />
    )
-   list.find('Experience').prop('onChange')({ id: 1 })
+   list.find(Experience).prop('onChange')({ id: 1 })
 })
 
 it('calls for add when an experience needs to be added', () => {
    expect.assertions(1)
    const data = [{ id: 1 }]
    const list = shallow(<ExperienceList experiences={data} onAdd={() => expect(true).toBe(true)} />)
-   list.find('Experience').prop('onAdd')()
+   list.find(Experience).prop('onAdd')()
 })
