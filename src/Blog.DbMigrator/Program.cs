616ad0e0278d3bfeb8c029ce61de504628d557db
Ali Doustkani <ali.doostkany@gmail.com>
2019-10-02 18:25:00 +0330

    add console app for db migration

---
+++ src/Blog.DbMigrator/Program.cs
@@ -0,0 +1,43 @@
+﻿using Blog.Infrastructure;
+using System;
+
+namespace Blog.DbMigrator
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string server, db, username, psw;
+         if (args.Length == 4)
+         {
+            server = args[0];
+            db = args[1];
+            username = args[2];
+            psw = args[3];
+         }
+         else
+         {
+            Console.WriteLine("Server:");
+            server = Console.ReadLine();
+
+            Console.WriteLine("Database Name:");
+            db = Console.ReadLine();
+
+            Console.WriteLine("Username:");
+            username = Console.ReadLine();
+
+            Console.WriteLine("Password:");
+            psw = Console.ReadLine();
+         }
+
+         try
+         {
+            Migrator.Migrate(server, db, username, psw);
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
+         }
+      }
+   }
+}
---
+++ src/Blog.Infrastructure/Migrator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Infrastructure
+{
+   public static class Migrator
+   {
+      public static void Migrate(string server, string database, string username, string password)
+      {
+         var connectionString = $"Data Source={server};Initial Catalog={database};User ID={username};Password={password};Connect Timeout=60;";
+         var optionsBuilder = new DbContextOptionsBuilder();
+         optionsBuilder.UseSqlServer(connectionString);
+         var context = new BlogContext(optionsBuilder.Options);
+         context.Database.Migrate();
+      }
+   }
+}
